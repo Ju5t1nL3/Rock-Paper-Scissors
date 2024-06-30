61e2ae4d69b2fede6c0df9b0ffac4ed231e72fc8
Justin Le <149338419+Ju5t1nL3@users.noreply.github.com>
2024-06-29 22:59:04 -0500

    Update RockPaperScissors.py

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -1,6 +1,13 @@
+"""
+This is a simple game of rock paper scissors against a bot.
+"""
+
 import random
 
 def get_choices():
+  """
+  Receives the choice of the player
+  """
   player_choice = input("Enter a choice (rock, paper, scissors): ")
   options = ["rock", "paper", "scissors"]
   computer_choice = random.choice(options)
@@ -8,6 +15,9 @@ def get_choices():
   return choices
 
 def check_win(player, computer):
+  """
+  Checks which player won
+  """
   print (f"You chose {player}, computer chose {computer}")
   if player == computer:
     return "It's a tie!"
@@ -27,6 +37,7 @@ def check_win(player, computer):
     else:
       return "Rock smashes scissors! You lose."
 
+#Runs game
 choices = get_choices()
 result = check_win(choices["player"], choices["computer"])
 print(result)
